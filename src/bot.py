e5b86d0de4464ce290dca8712be97d74df9a1d58
carlostoek <carlostoek@gmail.com>
2025-07-16 19:29:33 -0600

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -1,11 +1,15 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from .config import BOT_TOKEN
-from .handlers.admin import admin_router
-from .handlers.user import user_router
+from aiogram.client.default import DefaultBotProperties
+from config import BOT_TOKEN
+from handlers.admin import admin_router
+from handlers.user import user_router
 
 async def main():
-    bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
+    bot = Bot(
+        token=BOT_TOKEN,
+        default=DefaultBotProperties(parse_mode="HTML")
+    )
     dp = Dispatcher()
     dp.include_router(admin_router)
     dp.include_router(user_router)
