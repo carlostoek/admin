09216607cd3027e8aec58bbe6bf46ac2b0724c77
carlostoek <carlostoek@gmail.com>
2025-07-18 08:09:54 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from pydantic_settings import BaseSettings, validator
+from pydantic_settings import BaseSettings
 from pydantic import validator
 load_dotenv()
 
