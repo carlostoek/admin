03f6ae5294e94dde0031e7d4a4f4ddead1435d95
carlostoek <carlostoek@gmail.com>
2025-07-18 08:34:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from database import init_db
 from handlers import vip, free, common
 from middlewares.logging import LoggingMiddleware
 from utils.scheduler import start_scheduler
-from handlers.free import free_router  # Importar el router específico
+from handlers.free import free  # Importar el router específico
 
 logging.basicConfig(
     level=logging.INFO,
